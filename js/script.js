3703ce936953664acb2c502d8ca76b4cff6376e6
Danny Vega <dannyevega@gmail.com>
2015-10-20 15:01:33 -0700

    add all images for shoes. add ququestion objects in js file for shoe info. adjust styling and have working non-interactive version ready

--- js/script.js
+++ js/script.js
@@ -1,14 +1,14 @@
 $(document).ready(function(){
 	$('.game-area').hide();
-	$('.answer-overlay').hide();
+	$('.overlay').hide();
 	$('#start').click(function(){
 		$('.introduction').fadeOut();
 		$('.game-area').fadeIn(3000);
 	})
 
-	$('#answer-submit').submit(function(e){
+	$('.user-submit').submit(function(e){
 		e.preventDefault();
 		$('.game-area').hide();
-		$('.answer-overlay').fadeIn();
+		$('.overlay').fadeIn();
 	})
 });
